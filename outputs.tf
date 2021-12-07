cc975c2939f1ab0596fd194f7b2ce81b4490d4d1
Rick Jen <rijen@microsoft.com>
2021-12-06 22:10:28 -0800

    adding vnet link and outputs.tf

---
+++ outputs.tf
@@ -0,0 +1,16 @@
+
+output "container_registry_id" {
+  value = "${module.container-registry.container_registry_id}"
+}
+
+output "container_registry_admin_username" {
+  value = "${module.container-registry.container_registry_admin_username}"
+}
+
+output "container_registry_admin_password" {
+  value = "${module.container-registry.container_registry_admin_password}"
+}
+
+output "container_registry_private_endpoint_ip_addresses" {
+  value = "${module.container-registry.container_registry_private_endpoint_ip_addresses}"
+}
