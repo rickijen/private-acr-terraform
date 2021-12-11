60c54c1d623f15c4b0a1db0d95cabed141ceb7f4
Rick Jen <rijen@microsoft.com>
2021-12-10 17:17:22 -0800

    updated output

--- outputs.tf
+++ outputs.tf
@@ -3,17 +3,13 @@ output "container_registry_id" {
   value = "${module.container-registry.container_registry_id}"
 }
 
+# Admin user name is the same as registry name
 output "container_registry_admin_username" {
   value = "${module.container-registry.container_registry_admin_username}"
 }
 
-output "container_registry_admin_password" {
-  value = "${module.container-registry.container_registry_admin_password}"
-  sensitive = true
-}
-
-output "container_registry_private_endpoint_ip_addresses" {
-  value = "${module.container-registry.container_registry_private_endpoint_ip_addresses}"
+output "container_registry_name" {
+  value = "${module.container-registry.container_registry_admin_username}"
 }
 
 output "container_registry_private_dns_zone_domain" {
